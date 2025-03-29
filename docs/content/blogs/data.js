0d134275d3e0d4cf2bf2a6cf0af3b031e0957ef4
Legend <thesfmgod@gmail.com>
2025-03-28 22:40:17 -0400

    Blog Post #1: Changes & Tweaks!

--- docs/content/blogs/data.js
+++ docs/content/blogs/data.js
@@ -1,22 +1,41 @@
 const blogPosts = [
     {
-        title: "Test Blog Post - 3/28/2025",
+        title: "Blog Post #1: Changes & Tweaks! - 3/28/2025",
+
         shownContent: `
-            Hello, welcome to my second blog post! <br> <br>
-            I have made some changes to the blog page. The blog posts are now dynamically rendered from a data.js file instead of being hardcoded in the HTML. 
+            Hello, welcome to my first initial blog post! <br> <br>
+
+            I have made some changes to the ENTIRE WEBSITE. But I mainly got some work on the blog page. 
+            The blog posts are now dynamically rendered from a data.js file instead of being hardcoded in the HTML. 
             This allows for easier management and updates to the blog posts without having to modify the HTML directly.
             <br> <br>`,
 
         hiddenContent: `
             I have also added a "Show More" button that will reveal the hidden content of each blog post when clicked.
             This makes the page cleaner and allows users to focus on the main content first. <br> <br>
-            I will continue to work on improving the blog page and adding more features in the future. Stay tuned! <br> <br>
+
+            As for the Projects page & Main page, I have made some minor changes to the layout and design.
+            The Projects page now has a more organized layout when it comes to code. 
+            images are now softcoded and loads up instantly instead of taking 2-3 seconds to load up.
+            Theres a lot more I have to when it comes to the projects page, but for now, it is a lot better than before. <br> <br>
+
+            As for the main page, I have made some noticable changes to the layout.
+            I removed a lot of the '<strong>' tags within my little about me section so its more readable and less cluttered.
+            I also spaced out the text a *tad* bit so its not so cramped up on certain devices (most notably mobile devices).
+            I got rid of the contact links, because whats the point of having them when you can just click the icons on the bottom of the page lol.
+            And I also make the PDF icon look 'clickable' by adding a hover effect to it. <br> <br>
+
+            A lot was done today, and I am very happy with the results.
+
+            I will continue to work on improving the website and adding more features in the future. Stay tuned! <br> <br>
+
             <strong>Written By: Aaron Augustin</strong>
         `
     },
     
+    /*
     {
-        title: "Test Blog post - 3/23/2025",
+        title: "Test Blog post #1 - 3/23/2025",
         shownContent: `
             Hello, welcome to my first blog post! 
             I'm still trying to work on some backend stuff for this page so I may or may not get some good use out of this page very soon!
@@ -28,4 +47,5 @@ const blogPosts = [
             <strong>Written By: Aaron Augustin</strong>
         `
     }
+    */
 ];
