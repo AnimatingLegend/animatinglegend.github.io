0a3d610602318d3972496f8f9f6bdb4dfa6a522f
Legend <thesfmgod@gmail.com>
2025-03-29 23:51:55 -0400

    Source Code Overhaul

--- docs/src/js/projects.js
+++ docs/src/js/projects.js
@@ -6,7 +6,7 @@ const  projects = [
         <a title="Friday Night Funkin Old Github Repo" href="https://github.com/FunkinCrew/Funkin/tree/legacy/0.2.x" class="FNF" target="_blank"><strong>Friday Night Funkin's original engine</strong></a> contained a lot of bugs and errors at that time, and playing was frustrating. 
         Because I was bored and wanted to improve the experience, I decided to fix these bugs and implement my own modifications to improve the game. 
         I wrote this project in <strong>Haxe and HaxeFlixel</strong>, which allowed me to modify and optimize the game engine successfully.`,
-        image: 'assets/images/logos/FNF-Logo.png',
+        image: '../assets/images/logos/FNF-Logo.png',
         link:'/content/FDE-WEB-DEMO/'
     },
 
@@ -20,7 +20,7 @@ const  projects = [
         Legbot-Host expanded on these foundational skills, allowing us to implement more complex features. 
         It was an awesome learning experience and a good time that helped us improve our coding skills 
         Though I did cease to develop the bot sometime in 2022, it was a fantastic learning experience about how to go about developing bots and dealing with real-time APIs.`,
-        image: 'assets/images/logos/Discord-Logo.png',
+        image: '../assets/images/logos/Discord-Logo.png',
         link: null
     },
 
@@ -31,7 +31,7 @@ const  projects = [
         Since we frequently had to use <strong>Quizziz</strong> for schoolwork—which was both tedious and time-consuming—we figured, why not automate the process? 
         This script was written in <strong>JavaScript</strong> and worked by reading Quizziz's test data and automatically highlighting the correct answers. 
         Although the project was eventually discontinued and Quizziz patched the exploit, it was a great learning experience in understanding JavaScript strings and how web pages process data.`,
-        image: 'assets/images/logos/Quizizz-Logo.png',
+        image: '../assets/images/logos/Quizizz-Logo.png',
         link: null
     }
 ];
