e50eeedd840ea4ea3cd21e38adb1e761feaf128a
Legend <thesfmgod@gmail.com>
2025-03-31 00:50:27 -0400

    Added Animations || Backend Stuff || New Blog Post !!!

--- docs/src/js/data.js
+++ docs/src/js/data.js
@@ -1,5 +1,34 @@
 const blogPosts = [
     {
+        title: "Blog Post #2: Backend, FNF & Animations - 3/31/2025",
+
+        shownContent: `
+        Hello again, and welcome back to another blog post !! <br> <br>
+
+        Today I wanted to take a <strong>*little*</strong> step back from implementing new features and work more on backend stuff for my website. 
+        For starters, I did a massive overhaul on the source code. I wanted to keep things organized in folders instead of it being overwhelming and scattered all over the place.
+        So for users viewing the <a href="https://github.com/AnimatingLegend/animatinglegend.github.io" class="link" target="_blank">Portfolio Source Code</a> now, it should look WAY more cleaner and readable. <br> <br>
+        `,
+
+        hiddenContent: `
+        For seconds, I quickly wanted to mention that I started working on more backend stuff for data.js. 
+        My projects are now being rendered here aswell as my blog post's the file looks a little messy, but it keeps the rest of the website optimized and clean so im not complaining. <br> <br>
+
+        For thirds, I wanted to take a step back and work more on my engine 'Funkin-Definitive-Edition', because 1: it's WAS on an outdated version of haxe/haxeflixel, and 2: the HTML5 version of the game was having issues running.
+        So I merged the engine from Haxe 4.1.5, - Haxe 4.3.5. Along side that, I have to update each and every single haxe depenency I had, and along that came a WHOLE bunch of compliling issues. 
+        Now for people who want to compile my engine using the latest version of haxe, I saved you the trouble and made my haxe dependencies <a href="https://github.com/AnimatingLegend/Funkin-Haxe-Libraries" class="link" target="_blank">open sourced!!</a> 
+        I also completely removed the use of Polymod for my engine, because my engine isn't intended for modding purposes, more as in performance and coding experience. The engine is still undergoing bug maintenance but I squashed a lot of the bugs today, and it should be playable across on my website now! <br <br>
+
+        And to conclude this short blog, I wanted to mention that I implmented my animations onto my website! I was holding it off for so long, and for good reason.
+        But now that the website is in good shape, everything is polished and optimized, I decided to start adding them here. Theres only 3 currently up right now, but there is more to come in the following weeks! <br> <br>
+
+        That basically covers up about 2 days worth of progress. Thank you for sticking with me, and there will be more updates to in the following days! <br> <br>
+
+        <strong>Written By Aaron Augustin</strong>
+        `
+    },
+
+    {
         title: "Blog Post #1: Changes & Tweaks! - 3/28/2025",
 
         shownContent: `
@@ -20,8 +49,8 @@ const blogPosts = [
             Theres a lot more I have to when it comes to the projects page, but for now, it is a lot better than before. <br> <br>
 
             As for the main page, I have made some noticable changes to the layout.
-            I removed a lot of the bold text within my little about me section so its more readable and less cluttered.
-            I also spaced out the text a *tad* bit so it's not so cramped up on certain devices (most notably mobile devices).
+            I removed a lot of the <strong>bold</strong> text within my little about me section so its more readable and less cluttered.
+            I also spaced out the text a <strong>*tad*</strong> bit so it's not so cramped up on certain devices (most notably mobile devices).
             I got rid of the contact links, because whats the point of having them when you can just click the icons on the bottom of the page? lol.
             And I also made the PDF icon look 'clickable' by adding a hover effect to it. <br> <br>
 
@@ -32,20 +61,67 @@ const blogPosts = [
             <strong>Written By: Aaron Augustin</strong>
         `
     },
-    
-    /*
+];
+
+const animProjects = [
     {
-        title: "Test Blog post #1 - 3/23/2025",
-        shownContent: `
-            Hello, welcome to my first blog post! 
-            I'm still trying to work on some backend stuff for this page so I may or may not get some good use out of this page very soon!
-            If you take a look at the <a href="https://github.com/AnimatingLegend/animatinglegend.github.io/blob/main/docs/blog.html" class="blog-links" target="_blank"><strong>source code</strong></a>, the way I'm currently writing my blogs is horrendous. <br> <br>`,
-        hiddenContent: `
-            Later down the weeks, blogs will be written in a different format instead of straight <strong>HTML</strong> and <strong>CSS</strong>. 
-            And the code for this page will undergo massive improvements and better optimization, <strong>frontend & backend</strong>. <br> <br>
-            Thanks for reading! <br> <br>
-            <strong>Written By: Aaron Augustin</strong>
-        `
+        title: `Video Games <br> [FLASH ANIMATION]`,
+        video: `../assets/videos/Video_Games.mp4`
+    },
+
+    {
+        title: `Bensons Crank <br> [REGULAR SHOW ANIMATION]`,
+        video: `../assets/videos/Bensons_Crank.mp4`
+    },
+
+    {
+        title: `Mexican Joker <br> [SOUTH PARK TEST ANIMATION]`,
+        video: `../assets/videos/Mexican_Joker_Test.mp4`
     }
-    */
+];
+
+const codingProjects = [
+    // Funkin-Definitive-Edition
+    {
+        title: '<a title="Funkin-Definitive-Edition Github Repo" href="https://github.com/AnimatingLegend/Funkin-Definitive-Edition" class="FDE" target="_blank">Funkin-Definitive-Edition</a>',
+        description: `My project, Funkin' Definitive Edition, was created on August 12, 2022. 
+        <a title="Friday Night Funkin Old Github Repo" href="https://github.com/FunkinCrew/Funkin/tree/legacy/0.2.x" class="FNF" target="_blank"><strong>Friday Night Funkin's original engine</strong></a> contained a lot of bugs and errors at that time, and playing was frustrating. 
+        Because I was bored and wanted to improve the experience, I decided to fix these bugs and implement my own modifications to improve the game. 
+        I wrote this project in <strong>Haxe and HaxeFlixel</strong>, which allowed me to modify and optimize the game engine successfully.`,
+        image: '../assets/images/logos/FNF-Logo.png',
+        link:'/content/FDE-WEB-DEMO/'
+    },
+
+    // animatinglegend.github.io
+    {
+        title: `<a title="animatinglegend.github.io Official Github Repo" href="https://github.com/AnimatingLegend/animatinglegend.github.io" class="GIT" target="_blank">animatinglegend.github.io</a>`,
+        description: `<strong>Click the title, or the icon above to view the offical repository for this website !!!</strong>`,
+        image: '../assets/images/logos/Github-Logo.png',
+        link: null
+    },
+
+    // Colorbot / Legbot-Host
+    {
+        title: '<strong><a title="ColorBot-Host Github Repo" class="colorbot-host" href="https://github.com/colorlol/colorbothost" target="_blank">ColorBot</a> / <a title="Legbot-Host Github Repo" class="legbot-host" href="https://github.com/AnimatingLegend/legbothost" target="_blank">Legbot-Host</a></strong>',
+        description: `Two projects in the same category, <strong>Legbot-Host, and Colorbot-host</strong>. 
+        Legbot-host was a Discord bot that I made with my friend <a title="colorlol" href="https://github.com/colorlol" class="colorlol" target="_blank"><strong>Tamesh</strong></a> during the 2021 pandemic. 
+        We were spending so much time on Discord with not much else going on, so we figured we'd learn <strong>Node.js</strong> and create a bot that could tell jokes, display the weather, and even generate images of pets. 
+        This experience built upon my earlier work with Discord bots, including Tamesh's Colorbot, where I first explored basic bot functionality. 
+        Legbot-Host expanded on these foundational skills, allowing us to implement more complex features. 
+        It was an awesome learning experience and a good time that helped us improve our coding skills 
+        Though I did cease to develop the bot sometime in 2022, it was a fantastic learning experience about how to go about developing bots and dealing with real-time APIs.`,
+        image: '../assets/images/logos/Discord-Logo.png',
+        link: null
+    },
+
+    // Quizizz-Cheat
+    {
+        title: '<a title="Quizizz-cheat Github Repo" href="https://github.com/colorlol/quizizzcheat" class="quiz" target="_blank">Quizizz Cheat</a>',
+        description: `During that same time in 2021, <a title="colorlol" href="https://github.com/colorlol" class="colorlol" target="_blank"><strong>Tamesh</strong></a> and I also created Quizziz-Cheat, a small project that taught us the basics of <strong>inspect element</strong> and how to manipulate code in real time. 
+        Since we frequently had to use <strong>Quizziz</strong> for schoolwork—which was both tedious and time-consuming—we figured, why not automate the process? 
+        This script was written in <strong>JavaScript</strong> and worked by reading Quizziz's test data and automatically highlighting the correct answers. 
+        Although the project was eventually discontinued and Quizziz patched the exploit, it was a great learning experience in understanding JavaScript strings and how web pages process data.`,
+        image: '../assets/images/logos/Quizizz-Logo.png',
+        link: null
+    }
 ];
--- docs/src/js/projects.js
+++ docs/src/js/projects.js
@@ -1,54 +1,9 @@
-const  projects = [
-    // Funkin-Definitive-Edition
-    {
-        title: '<a title="Funkin-Definitive-Edition Github Repo" href="https://github.com/AnimatingLegend/Funkin-Definitive-Edition" class="FDE" target="_blank">Funkin-Definitive-Edition</a>',
-        description: `My project, Funkin' Definitive Edition, was created on August 12, 2022. 
-        <a title="Friday Night Funkin Old Github Repo" href="https://github.com/FunkinCrew/Funkin/tree/legacy/0.2.x" class="FNF" target="_blank"><strong>Friday Night Funkin's original engine</strong></a> contained a lot of bugs and errors at that time, and playing was frustrating. 
-        Because I was bored and wanted to improve the experience, I decided to fix these bugs and implement my own modifications to improve the game. 
-        I wrote this project in <strong>Haxe and HaxeFlixel</strong>, which allowed me to modify and optimize the game engine successfully.`,
-        image: '../assets/images/logos/FNF-Logo.png',
-        link:'/content/FDE-WEB-DEMO/'
-    },
+// This is for implementing coding projects
+const codingContainer = document.getElementById('coding-container');
 
-    // animatinglegend.github.io
-    {
-        title: `<a title="animatinglegend.github.io Official Github Repo" href="https://github.com/AnimatingLegend/animatinglegend.github.io" class="GIT" target="_blank">animatinglegend.github.io</a>`,
-        description: `<strong>Click the title, or the icon above to view the offical repository for this website !!!</strong>`,
-        image: '../assets/images/logos/Github-Logo.png',
-        link: null
-    },
-
-    // Colorbot / Legbot-Host
-    {
-        title: '<strong><a title="ColorBot-Host Github Repo" class="colorbot-host" href="https://github.com/colorlol/colorbothost" target="_blank">ColorBot</a> / <a title="Legbot-Host Github Repo" class="legbot-host" href="https://github.com/AnimatingLegend/legbothost" target="_blank">Legbot-Host</a></strong>',
-        description: `Two projects in the same category, <strong>Legbot-Host, and Colorbot-host</strong>. 
-        Legbot-host was a Discord bot that I made with my friend <a title="colorlol" href="https://github.com/colorlol" class="colorlol" target="_blank"><strong>Tamesh</strong></a> during the 2021 pandemic. 
-        We were spending so much time on Discord with not much else going on, so we figured we'd learn <strong>Node.js</strong> and create a bot that could tell jokes, display the weather, and even generate images of pets. 
-        This experience built upon my earlier work with Discord bots, including Tamesh's Colorbot, where I first explored basic bot functionality. 
-        Legbot-Host expanded on these foundational skills, allowing us to implement more complex features. 
-        It was an awesome learning experience and a good time that helped us improve our coding skills 
-        Though I did cease to develop the bot sometime in 2022, it was a fantastic learning experience about how to go about developing bots and dealing with real-time APIs.`,
-        image: '../assets/images/logos/Discord-Logo.png',
-        link: null
-    },
-
-    // Quizizz-Cheat
-    {
-        title: '<a title="Quizizz-cheat Github Repo" href="https://github.com/colorlol/quizizzcheat" class="quiz" target="_blank">Quizizz Cheat</a>',
-        description: `During that same time in 2021, <a title="colorlol" href="https://github.com/colorlol" class="colorlol" target="_blank"><strong>Tamesh</strong></a> and I also created Quizziz-Cheat, a small project that taught us the basics of <strong>inspect element</strong> and how to manipulate code in real time. 
-        Since we frequently had to use <strong>Quizziz</strong> for schoolwork—which was both tedious and time-consuming—we figured, why not automate the process? 
-        This script was written in <strong>JavaScript</strong> and worked by reading Quizziz's test data and automatically highlighting the correct answers. 
-        Although the project was eventually discontinued and Quizziz patched the exploit, it was a great learning experience in understanding JavaScript strings and how web pages process data.`,
-        image: '../assets/images/logos/Quizizz-Logo.png',
-        link: null
-    }
-];
-
-const projectContainer = document.getElementById('projects-container');
-
-projects.forEach(project => {
+codingProjects.forEach(project => {
     const projectDiv = document.createElement('div');
-    projectDiv.classList.add('project');
+    projectDiv.classList.add('coding-project');
 
     const logoClass = project.image.includes('FNF-Logo') ? 'fnf-logo' :
                       project.image.includes('Discord-Logo') ? 'discord-logo' :
@@ -67,5 +22,37 @@ projects.forEach(project => {
         <p class="project-desc">${project.description}</p> <br>
     `;
 
-    projectContainer.appendChild(projectDiv);
+    codingContainer.appendChild(projectDiv);
+});
+
+// This is for implementing animation projects
+const animationContainer = document.getElementById('anim-container');
+
+animProjects.forEach(animation => {
+    const videoDiv = document.createElement('div');
+    videoDiv.classList.add('anim-project');
+
+    const videoHTML = `
+        <video controls loading="lazy"> 
+            <source src="${animation.video}" type="video/mp4">
+            Your browser does not support the video tag.
+        </video>
+    `;
+
+    videoDiv.innerHTML = `
+        <h2>${animation.title}</h2>
+        ${videoHTML}
+        <!-- <p class="animation-desc">${animation.description}</p> -->
+    `;
+
+    videoDiv.addEventListener('click', () => {
+        const video = videoDiv.querySelector('video');
+
+        if (video.paused) 
+            video.play; 
+        else 
+            video.pause;
+    });
+
+    animationContainer.appendChild(videoDiv);
 });
