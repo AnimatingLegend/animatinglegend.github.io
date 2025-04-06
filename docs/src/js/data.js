220c42f1c3aa470adad77e988a536d469ad914b4
Legend <thesfmgod@gmail.com>
2025-04-06 13:10:14 -0400

    Update data.js

--- docs/src/js/data.js
+++ docs/src/js/data.js
@@ -148,7 +148,7 @@ const codingProjects = [
         <a title="Friday Night Funkin Old Github Repo" href="https://github.com/FunkinCrew/Funkin/tree/legacy/0.2.x" class="FNF" target="_blank"><strong>Friday Night Funkin's original engine</strong></a> contained a lot of bugs and errors at that time, and playing was frustrating. 
         Because I was bored and wanted to improve the experience, I decided to fix these bugs and implement my own modifications to improve the game. 
         I wrote this project in <strong>Haxe and HaxeFlixel</strong>, which allowed me to modify and optimize the game engine successfully.
-        <strong>If you want to view the DEBUG WEB version of the engine <a href="/content/FDE-WEB-DEMO-DEBUG" target="_blank">click here!!</a></strong>`,
+        <strong>If you want to view the DEBUG WEB version of the engine <a title="FDE-WEB-DEMO-DEBUG" href="/content/FDE-WEB-DEMO-DEBUG" class="FNF" target="_blank">click here!!</a></strong>`,
         image: '../assets/images/logos/FNF-Logo.png',
         link:'/content/FDE-WEB-DEMO/'
     },
