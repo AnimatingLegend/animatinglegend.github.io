15523fde65c73df1a6971a0fa8aa63c7edf5999d
Legend <thesfmgod@gmail.com>
2025-04-18 22:39:56 -0400

    Update blog-data.js || Fixed any grammatical errors, and re-worded certain sentences

--- docs/src/javascript/blog-data.js
+++ docs/src/javascript/blog-data.js
@@ -8,19 +8,19 @@ const blog_posts = [
         shown_content: `
         <strong class="blog-headings-large">Introduction</strong> <br>
         Hello all, and happy Friday! 
-        I just wanted to quickly inform you that I got rid of all previous blog post's because I personally thought they were lazy and horribly written, so I'm here to make a well thought out and lengthy blog post for you guys.
-        Think of it as a "fresh start", like as I haven't made any blog post's prior to this new one...
+        I just wanted to quickly let you know that I deleted all my previous blog posts because I personally felt they were lazy and poorly written. So, I'm here to give you a well-thought-out and lengthy blog post instead.
+        Think of it as a "fresh start," as if I've never made any blog posts before this one...
         <br> <br>`,
         hidden_content: `
-        <strong class="blog-headings-large">Section 1 - Website Source code</strong> <br>
-        But with that being said, I hope all is well, and I mainly wanna talk about the progress I've made with the site, and the overall progress I've mad with my FNF Engine: <strong>Funkin-Definitive-Edition</strong>.
-        For starters, I did a MASSIVE overhaul on the websites source code, in terms of functionality, and the way everything is displayed.
-        I completely rewritten a majority of the backend code for the <strong><a href="projects.html" class="link">Projects</a></strong> Section of the website.
-        The way projects were being displayed before was really confusing and messy in terms of how it's written backend wise.
-        The data was broken up into 4 sections <strong>Title, Description, Image & Link</strong>.
-        The way I went about programming it was by hardcoding a lot of the HTML elements into the 4 sections, which looking back at it was such a hassle...
-        I later took note of this, and decided to softcode a lot of the elements so I'm not typing as much HTML into those sections.
-        I also decided to break the sections down into 10 so everything is neat, easier to read and more easier to implement!
+        <strong class="blog-headings-large">Section 1 - Website Source Code</strong> <br>
+        With that being said, I hope all is well! I mainly want to talk about the progress I've made on the webite and the overall progress I've made with my FNF engine: <strong>Funkin-Definitive-Edition</strong>.
+        To start, I did a MASSIVE overhaul on the website's source code—both in terms of functionality and how everything is displayed.
+        I completely rewrote a majority of the backend code for the <strong><a href="projects.html" class="link">Projects</a></strong> section of the website.
+        The way projects were displayed before was confusing and messy, especially in terms of how it was written on the backend.
+        The data was broken up into four sections: <strong>Title, Description, Image & Link</strong>.
+        Previously, I hardcoded a lot of the HTML elements into those four sections, which in hindsight was a hassle...
+        I later realized this and decided to softcode most of the elements, so I wouldn't have to type as much HTML into those sections.
+        I also decided to break the sections down into 8 - 10 slots, making everything neater, easier to read, and much more easier to implement!
 
         <br> <br>
         <strong class="blog-headings-medium">Before:</strong> <br> <br>
@@ -31,7 +31,7 @@ const blog_posts = [
         <img src="../content/blogs/2025-18-04/project_code_AFTER.png" class="thumbnail img-content-2" onclick="openLightbox(src)">
 
         <br> <br>
-        And as for the "Source Code Overhaul", I rearranged a lot of file into specific folder/libraries so the workspace explorer looks more cleaner and easier to navagate.
+        As for the "Source Code Overhaul," I reorganized a lot of files into specific folders/libraries so the workspace explorer looks cleaner and is easier to navigate.
 
         <br> <br>
         <strong class="blog-headings-medium">Old Workspace:</strong> <br> <br>
@@ -43,15 +43,15 @@ const blog_posts = [
 
         <br> <br>
         <strong class="blog-headings-large">Section 2 - Funkin': Definitive Edition</strong> <br>
-        For seconds, I just wanted to talk more about Funkin-Definitive-Edition. (by which you can <strong><a title="Funkin-Definitive-Edition - WEB DEMO" href="/content/bin-release" class="link" target="_blank">play now!</a></strong>)
-        For the past couple of weeks, I've been hard at work trying to tweak up a lot of the game's minor bugs / glitches in order for it to be <strong>HTML</strong> friendly and "website ready".
-        For starters, the game would always crash either on the boot-up screen, or when loading into a song.
-        When looking further into it, I came to a conclusion figuring out it had something to do with how libraries work with my engine... 
-        So I basically had to rewrite the file locations for songs and stages in <strong>Project.xml</strong> and everything was loading up pretty well.
-        As for any other information about the game, FDE now supports <strong>Haxe v4.2.5</strong>, and <strong>Haxeflixel v4.9.0</strong>.
-        The only reason I updated versions was because people were having building issues, and the previous haxe version the game was running on, (Haxe v4.1.5) was EXTREMELY OUTDATED.
-        If you want to build the game yourself or learn more about this issue then <strong><a title="Funkin-Definitive-Edition - BUILDING GUIDE" href="https://github.com/AnimatingLegend/Funkin-Definitive-Edition/blob/master-final/docs/guides/building.md" target="_blank" class="link">read this guide</a></strong>.
-        There still are some bugs, but they're pretty miniscule, and I'm actively working on the <strong><a title="Funkin-Definitive-Edition - HTML5 SOURCE CODE" href="https://github.com/AnimatingLegend/Funkin-Definitive-Edition/tree/master-HTML5" target="_blank" class="link">HTML5 version</a></strong> of the engine, so bugs are constantly getting squashed.
+        Secondly, I just wanted to talk more about Funkin-Definitive-Edition (which you can <strong><a title="Funkin-Definitive-Edition - WEB DEMO" href="/content/bin-release" class="link" target="_blank">play now!</a></strong>).
+        Over the past couple of weeks, I've been hard at work fixing a lot of the game's minor bugs and glitches to make it <strong>HTML</strong>-friendly and “website ready.”
+        For starters, the game used to crash either on the boot-up screen or while loading into a song.
+        After digging into it, I realized the issue had to do with how libraries were being handled in my engine...
+        So I had to rewrite the file paths for songs and stages in <strong>Project.xml</strong> — and now everything loads properly.
+        In terms of compatibility, FDE now supports <strong>Haxe v4.2.5</strong> and <strong>Haxeflixel v4.9.0</strong>.
+        The only reason I updated versions is because people were having build issues, and the previous Haxe version (v4.1.5) was EXTREMELY outdated.
+        If you want to build the game yourself or learn more about this issue, <strong><a title="Funkin-Definitive-Edition - BUILDING GUIDE" href="https://github.com/AnimatingLegend/Funkin-Definitive-Edition/blob/master-final/docs/guides/building.md" target="_blank" class="link">read this guide</a></strong>.
+        There are still a few bugs, but they're pretty minor, and I'm actively working on the <strong><a title="Funkin-Definitive-Edition - HTML5 SOURCE CODE" href="https://github.com/AnimatingLegend/Funkin-Definitive-Edition/tree/master-HTML5" target="_blank" class="link">HTML5 version</a></strong> of the engine, so bugs are constantly being squashed.
 
         <br> <br>
         <strong class="blog-headings-medium">Photo Of Gameplay:</strong> <br> <br>
@@ -59,8 +59,8 @@ const blog_posts = [
 
         <br> <br>
         <strong class="blog-headings-large">Conclusion</strong> <br>
-        That pretty much wraps up everything I wanted to talk about in terms of my website, and my game engine.
-        Thank you for reading this semi-long blog post, and hopefully there will be more in the mere future! <strong>(*°∀°)=3</strong>
+        That pretty much wraps up everything I wanted to talk about in terms of the website and the game engine.
+        Thanks for reading this semi-long blog post — hopefully there will be more to come in the near future! <strong>(*°∀°)=3</strong>
 
         <br> <br>
         <strong class="blog-headings-medium">SONG OF THE WEEK:</strong> <br>
